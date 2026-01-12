44ceb0e52a9e67aa20cfafa876cf1d21656d97a8
Sebastien King <sebastienking0511@gmail.com>
2026-01-12 12:42:14 +1000

    have added colour to canvas

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ console.log(game);
 game.width = 800;
 game.height = 800;
 const ctx = game.getContext("2d");
-console.log(ctx)
-ctx.fillRect(0, 0, game.width, game.height)
-ctx.fillStyle = "#559955"
+console.log(ctx);
+ctx.fillStyle = "#101010";
+ctx.fillRect(0, 0, game.width, game.height);
+ctx.fillStyle = "#559955";
 ctx.fillRect(0, 0, 100, 100);
