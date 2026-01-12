755f638968cc04f4abd8225b33209c2227b6b78f
Sebastien King <sebastienking0511@gmail.com>
2026-01-12 12:53:07 +1000

    have called func clear and point i called point to 100, 100

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ console.log(ctx);
 function clear() {
     ctx.fillStyle = BACKGROUND;
     ctx.fillRect(0, 0, game.width, game.height);
-}
+};
 
 function point(x, y) {
     ctx.fillStyle = FOREGROUND;
     ctx.fillRect(x, y, 100, 100);
-}
+};
+
+clear();
+point(100, 100);
