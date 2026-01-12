3994070379d1f41fa18daa8e1e20934f8931f0a1
Sebastien King <sebastienking0511@gmail.com>
2026-01-12 12:50:22 +1000

    have moved the BG and FG into funcs clear & point

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ game.width = 800;
 game.height = 800;
 const ctx = game.getContext("2d");
 console.log(ctx);
-ctx.fillStyle = BACKGROUND;
-ctx.fillRect(0, 0, game.width, game.height);
-ctx.fillStyle = FOREGROUND;
-ctx.fillRect(0, 0, 100, 100);
+
+function clear() {
+    ctx.fillStyle = BACKGROUND;
+    ctx.fillRect(0, 0, game.width, game.height);
+}
+
+function point(x, y) {
+    ctx.fillStyle = FOREGROUND;
+    ctx.fillRect(0, 0, 100, 100);
+}
