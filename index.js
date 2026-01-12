9a6258d5f13ae78e2c17f91c6e35eae2123a8dd9
Sebastien King <sebastienking0511@gmail.com>
2026-01-12 12:44:25 +1000

    have change the colour of the green box to lighter

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const ctx = game.getContext("2d");
 console.log(ctx);
 ctx.fillStyle = "#101010";
 ctx.fillRect(0, 0, game.width, game.height);
-ctx.fillStyle = "#559955";
+ctx.fillStyle = "#55ff55";
 ctx.fillRect(0, 0, 100, 100);
