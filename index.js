a63bc0e753fdcd47ee604a5771240fc08ecdb0a6
Sebastien King <sebastienking0511@gmail.com>
2026-01-12 12:47:41 +1000

    have stored the colours in variables now

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const BACKGROUND = "#101010";
+const FOREGROUND = "#55ff55";
+
 console.log(game);
 game.width = 800;
 game.height = 800;
 const ctx = game.getContext("2d");
 console.log(ctx);
-ctx.fillStyle = "#101010";
+ctx.fillStyle = BACKGROUND;
 ctx.fillRect(0, 0, game.width, game.height);
-ctx.fillStyle = "#55ff55";
+ctx.fillStyle = FOREGROUND;
 ctx.fillRect(0, 0, 100, 100);
